7d794b0160d140838c6bffc3efae6d81c755050c
Laibah-Shahid <hassanshahid234@gmail.com>
2024-02-15 22:32:31 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+let s1bag = document.getElementById("s1bag");
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+        document.getElementById("s1bag").classList.toggle("hide");
+    })  
+}
+
+if (close) {
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active'); 
+        s1bag.classList.remove("hide");
+    })
+}
